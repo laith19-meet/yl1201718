142c8cd3dc30c2d9351619aabf5b547986e498ca
sarah19-meet <sarah19@meet.mit.edu>
2018-01-10 20:08:20 +0200

    collision

--- lab 3/week8.py
+++ lab 3/week8.py
@@ -9,7 +9,9 @@ class ball(Turtle):
 		self.radius = radius
 		self.color(color)
 		self.speed(speed)
-
+	def move (self,x,y,dx,dy):
+		self.goto(x, y)
+		self.goto(dx,dy)
 circle1= ball(25,"black",6)
 circle1.pu()
 circle1.forward(100)
@@ -19,7 +21,9 @@ circles= []
 circles.append(circle1)
 circles.append(circle2)
 print(circles)
-
+pu()
+circle1.move(0, 0 , 15 , 20)
+circle2.move(15,15 , -17 ,-30)
 def check_collision(circle1, circle2):
 	x1=circle1.xcor()
 	y1=circle1.ycor()
@@ -32,7 +36,10 @@ def check_collision(circle1, circle2):
 	z=b+f
 	d=math.sqrt(z)
 	print(d)
+
 	
+
+
 	
 
 
